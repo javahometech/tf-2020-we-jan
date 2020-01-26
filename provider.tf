818e5fce822cd44374fe308931b0ec44f37774f0
Hari Kammana <hari.kammana@gmail.com>
2020-01-26 12:16:38 +0530

    terraform-1

--- provider.tf
+++ provider.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   region  = "ap-south-1"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "javahome-2020-tf"
+    key    = "terraform.tfstate"
+    region = "ap-south-1"
+    dynamodb_table = "javahome-tf"
+  }
+}
--- vpc.tf
+++ vpc.tf
@@ -7,3 +7,15 @@ resource "aws_vpc" "main" {
     Batch = "Weekend"
   }
 }
+
+
+resource "aws_subnet" "main" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-south-1a"
+  tags = {
+    Name = "Subnet-1"
+    Batch = "Weekend"
+    Location = "Banglore"
+  }
+}
